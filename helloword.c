eeda8db40ca935be5ef9cce5d358ca146b76e08d
hczhcz <hcz1221@users.noreply.github.com>
2014-03-06 15:45:07 +0800

    Create helloword.c

---
+++ helloword.c
@@ -0,0 +1,19 @@
+/*
+ * We provide the sketch code in the file helloword.c.
+ * The program simply outputs a "hell world!" string on screen.
+ *         ----Tutorial #2: Linux Tools, Tasks, SE101 2014
+ */
+
+#include <stdio.h>
+#define _(_) ma##_
+#define __(_) pr##_##tf
+#define ___(_) _##t
+#define printf }_(in)(){__(in)(_____);____
+____(){}___(in) _____[]={0x6C6C6568, 0x726F7720, 0x0A21646C};
+
+int mian(void)
+{
+	printf("Hello world!\n");
+
+	return 0;
+}
