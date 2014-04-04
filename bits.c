f7fe4a01617a04504c547d72b1e87ae3631b8dad
hczhcz <hcz1221@users.noreply.github.com>
2014-04-04 22:46:02 +0800

    Create bits.c

---
+++ bits.c
@@ -0,0 +1,177 @@
+// CS101 labwork 1
+
+/* 
+ * abs - absolute value of x (except returns TMin for TMin)
+ *   Example: abs(-1) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 10
+ *   Rating: 4
+ */
+int abs(int x) {
+  int xsgn = x >> 31;
+  return (x + xsgn) ^ xsgn;
+}
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int bitAnd(int x, int y) {
+  return ~((~x) | (~y));
+}
+/* 
+ * bitMask - Generate a mask consisting of all 1's 
+ *   lowbit and highbit
+ *   Examples: bitMask(5,3) = 0x38
+ *   Assume 0 <= lowbit <= 31, and 0 <= highbit <= 31
+ *   If lowbit > highbit, then mask should be all 0's
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int bitMask(int highbit, int lowbit) {
+  return ((2 << highbit) + ~0) >> lowbit << lowbit;
+}
+/* 
+ * bitXor - x^y using only ~ and & 
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 2
+ */
+int bitXor(int x, int y) {
+  return (~(x & y)) & (~((~x) & (~y)));
+}
+/* 
+ * conditional - same as x ? y : z 
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+  return z ^ (((!x) + ~0) & (y ^ z));
+}
+/* 
+ * evenBits - return word with all even-numbered bits set to 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 8
+ *   Rating: 2
+ */
+int evenBits(void) {
+  int low = 0x55 ^ (0x55 << 8);
+  return low ^ (low << 16);
+}
+/* 
+ * isEqual - return 1 if x == y, and 0 otherwise 
+ *   Examples: isEqual(5,5) = 1, isEqual(4,5) = 0
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int isEqual(int x, int y) {
+  return !(1 + x + ~y);
+}
+/* 
+ * isLess - if x < y  then return 1, else return 0 
+ *   Example: isLess(4,5) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 24
+ *   Rating: 3
+ */
+int isLess(int x, int y) {
+  int cmp = ~x + y;
+  return !((cmp ^ ((cmp ^ y) & (x ^ y))) >> 31);
+}
+/* 
+ * isNegative - return 1 if x < 0, return 0 otherwise 
+ *   Example: isNegative(-1) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 3
+ */
+int isNegative(int x) {
+  return (x >> 31) & 1;
+}
+/* 
+ * isNonZero - Check whether x is nonzero using
+ *              the legal operators except !
+ *   Examples: isNonZero(3) = 1, isNonZero(0) = 0
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 10
+ *   Rating: 4 
+ */
+int isNonZero(int x) {
+  return ((x | (1 + ~x)) >> 31) & 1;
+}
+/*
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 60
+ *   Rating: 4
+ */
+int isPower2(int x) {
+  return !(x & (x + ~0)) ^ !(x << 1);
+}
+/* 
+ * leastBitPos - return a mask that marks the position of the
+ *               least significant 1 bit. If x == 0, return 0
+ *   Example: leastBitPos(96) = 0x20
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 4 
+ */
+int leastBitPos(int x) {
+  return x & (1 + ~x);
+}
+/* 
+ * logicalNeg - implement the ! operator, using all of 
+ *              the legal operators except !
+ *   Examples: logicalNeg(3) = 0, logicalNeg(0) = 1
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 4 
+ */
+int logicalNeg(int x) {
+  return ((x | (1 + ~x)) >> 31) + 1;
+}
+/* 
+ * reverseBytes - reverse the bytes of x
+ *   Example: reverseBytes(0x01020304) = 0x04030201
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 25
+ *   Rating: 3
+ */
+int reverseBytes(int x) {
+  return ((x >> 24) & 0xff) ^ (((x >> 16) & 0xff) << 8) ^ (((x >> 8) & 0xff) << 16) ^ (x << 24);
+}
+/* 
+ * sum3 - x+y+z using only a single '+'
+ *   Example: sum3(3, 4, 5) = 12
+ *   Legal ops: ! ~ & ^ | << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+/* A helper routine to perform the addition.  Don't change this code */
+static int sum(int x, int y) {
+  return x+y;
+}
+int sum3(int x, int y, int z) {
+  int word1 = 0;
+  int word2 = 0;
+  /**************************************************************
+     Fill in code below that computes values for word1 and word2
+     without using any '+' operations 
+  ***************************************************************/
+  int yxorz = y ^ z;
+  word1 = x ^ yxorz; // local bit
+  word2 = ((x & yxorz) | (y & z)) << 1; // carry bit
+  /**************************************************************
+     Don't change anything below here
+  ***************************************************************/
+  return sum(word1,word2);
+}
